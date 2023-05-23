0ceeef55a4afa7eb639a5b77a6f56ecc782b5aad
Jelmer van Ofwegen <offie1@outlook.com>
2023-05-23 22:13:35 +0200

    Added authentication

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,4 +13,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
+        require('postcss-import'),
+        require('tailwindcss'),
     ]);
